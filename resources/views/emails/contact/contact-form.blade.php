cae25665c0014b6bf9c22a0f9b663c71b6169bfb
“SagorIslamOfficial” <alaminislam1274@gmail.com>
2022-12-14 20:30:04 +0600

    Changed .env and  email template

--- resources/views/emails/contact/contact-form.blade.php
+++ resources/views/emails/contact/contact-form.blade.php
@@ -1,15 +1,15 @@
 <x-mail::message>
 
-    <x-slot:subcopy>
-        Mail From <mark class="text-success">{{ $data['email'] }}</mark> - <em>Bashona Trading Ltd.</em>
-        <x-mail::subcopy>
+    <x-slot:subcopy class="pb-3">
+        Mail From - <mark class="text-success text-color-success">{{ $data['email'] }}</mark> - <em>{{ config('app.name') }}</em>
+        <x-mail::subcopy class="pb-3">
         <strong>Name:</strong> {{ $data['name'] }} <br>
         <strong>Email Address:</strong> {{ $data['email'] }} <br>
         <strong>Subject:</strong> {{ $data['subject'] }} <br>
         <strong>Message:</strong> {{ $data['message'] }} <br>
         </x-mail::subcopy>
 
-    Thanks
+    Thanks,
     {{ config('app.name') }}
     </x-slot:subcopy>
 
