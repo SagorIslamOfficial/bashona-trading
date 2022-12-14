ebd2ca88e11223a7a2852156480c97fd70a01c77
“SagorIslamOfficial” <alaminislam1274@gmail.com>
2022-12-14 21:17:08 +0600

    Changed login image

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -75,8 +75,8 @@
                     </div>
                     <div class="card col-md-5 text-white bg-primary py-5">
                         <div class="card-body text-center">
-                            <div style="width: 465px; height: 427px">
-                                <img src="{{ asset('backEnd/img/export-import.jpg') }}" alt="Login Form for bashonatrading.com">
+                            <div>
+                                <img style="width: 465px; height: 427px" src="{{ asset('backEnd/img/export-import.jpg') }}" alt="Login Form for bashonatrading.com">
                             </div>
                         </div>
                     </div>
