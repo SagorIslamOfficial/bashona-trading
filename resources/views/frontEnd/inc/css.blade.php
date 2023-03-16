41a9a7c773c4bb342de41c9d54cc775161030866
SagorIslamOfficial <alaminislam1274@gmail.com>
2023-03-17 03:38:42 +0600

    Lots of work

--- resources/views/frontEnd/inc/css.blade.php
+++ resources/views/frontEnd/inc/css.blade.php
@@ -5,12 +5,11 @@
     <title>@yield('title') {{ config('app.name') }}</title>
     <meta name="keywords" content="Bashona Trading Ltd"/>
     <meta name="description" content="Import, Export, IT and Design services">
-    <meta name="author" content="https://www.bashonatrading.com/">
+    <meta name="author" content="https://www.bashonatrading.com">
 
     <meta property="og:type"          content="website" />
     <meta property="og:title"         content="Bashona Trading Ltd" />
     <meta property="og:description"   content="Import, Export, IT and Design services" />
-    <meta property="og:image"         content="https://www.bashonatrading.com/" />
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{ asset('frontEnd/img/favicon.png') }}" type="image/x-icon"/>
@@ -18,8 +17,6 @@
     <!-- Mobile Metes -->
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
     <!-- Web Fonts  -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light&amp;display=swap" rel="stylesheet" type="text/css">
     <!-- Vendor CSS -->
     <link rel="stylesheet" href="{{ asset('frontEnd/vendor/bootstrap/css/bootstrap.min.css') }}">
