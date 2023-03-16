0342d337ab7d7ae21698b97d416e542b980af5b2
SagorIslamOfficial <alaminislam1274@gmail.com>
2023-03-17 04:02:32 +0600

    Lots of work

--- config/app.php
+++ config/app.php
@@ -54,9 +54,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://www.bashonatrading.com'),
+    'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL'),
+    'asset_url' => env('ASSET_URL', 'public/'),
 
     /*
     |--------------------------------------------------------------------------
